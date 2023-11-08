91b409124c847d5b513a869e613ea665cb4c0897
Arieloto <arielpizarrof@gmail.com>
2023-11-08 16:08:41 -0300

    entrega

---
+++ login.js
@@ -0,0 +1,44 @@
+
+function verificarCredenciales() {
+  const usuario = document.getElementById("usuario").value.toLowerCase();
+  const contraseña = document.getElementById("contraseña").value;
+  console.log(usuario)
+  if (usuario === "juanito" && contraseña === "coder") {
+    Swal.mixin({
+      toast: true,
+      position: "top",
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    }).fire({
+      icon: "success",
+      title: "Inicio de sesion exitosa!",
+    });
+    setTimeout(() => {
+      window.location.href = "index.html";
+      sessionStorage.setItem("nombreUsuario", usuario);
+      
+    }, 2100);
+    
+  } else {
+    Swal.mixin({
+      toast: true,
+      position: "top",
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    }).fire({
+      icon: "error",
+      title: "Credenciales incorrectas",
+    });
+  }
+}
+
--- main.js
+++ main.js
@@ -20,20 +20,104 @@ let inventario = []
 inventario.push(bien1)
 
 
-if (inventario.length > 0) {
- 
-  const datos = localStorage.getItem("Inmueble");
+const nombreUsuario = sessionStorage.getItem("nombreUsuario");
+const nombreUsuarioDiv = document.getElementById("nombreUsuarioDiv");
+const usuarioFormateado = nombreUsuario.charAt(0).toUpperCase() + nombreUsuario.slice(1);
+const mostrarMensajeInicioSesion = () => {
+  nombreUsuarioDiv.innerHTML = `
+    <div class="divusuario">
+      No has iniciado sesión.
+    </div>
+    <div>
+      <p><a href="login.html">Inicia sesión</a></p>
+    </div>
+  `;
+};
 
-  if (datos !== null && datos !== undefined) {
+if (nombreUsuario) {
+  nombreUsuarioDiv.innerHTML = `
+    <div>
+      <img src="img/user.png" alt="Logo" width=20px>
+    </div>
+    <div>
+      Bienvenido, ${usuarioFormateado}
+    </div>
+    <div class="salirsesion">
+      <img src="img/salir.png" alt="Logo" width=20px class="salir-imagen"><div class="textosalir">Cerrar sesion</div>
+    </div>
+  `;
+
+  const salirImagen = document.querySelector('.salir-imagen');
+  salirImagen.addEventListener('click', () => {
+  
+    sessionStorage.removeItem('nombreUsuario');
     
-    const bienesEnLocalStorage = JSON.parse(datos);
-    inventario = bienesEnLocalStorage;
-  }
+    mostrarMensajeInicioSesion();
+  });
+} else {
+  mostrarMensajeInicioSesion();
 }
+
+function obtenerInventario() {
+  return new Promise((resolve, reject) => {
+    if (inventario.length > 0) {
+      const datos = localStorage.getItem("Inmueble");
+
+      if (datos !== null && datos !== undefined) {
+        const bienesEnLocalStorage = JSON.parse(datos);
+        inventario = bienesEnLocalStorage;
+        resolve(inventario);
+      } else {
+        reject("No se encontraron datos en el almacenamiento local.");
+      }
+    } else {
+      reject("El inventario está vacío.");
+    }
+  });
+}
+
+
+obtenerInventario()
+  .then((inventarioActualizado) => {
+    Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 4000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    }).fire({
+      icon: "success",
+      title: "Datos cargados correctamente",
+    });
+  })
+  .catch((error) => {
+    Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 4000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    }).fire({
+      icon: "error",
+      title: "Error al cargar datos",
+    });
+  });
+
 
 //botones globales
 const filtrarBtn = document.getElementById("filtrar");
 filtrarBtn.addEventListener("click", () => {filtrarInventario();});
+
+const panelBtn = document.getElementById("panel");
+panelBtn.addEventListener("click", () => {panel();})
 
 const agregarBtn = document.getElementById("agregar");
 agregarBtn.addEventListener("click", () => {
@@ -48,6 +132,8 @@ mostrarInventario.addEventListener("click", () => {mostrarInv();});
 /////////////////////////////////////////////////////////////////////////////////
 function mostrarInv(){
 //muestro el inventario en la tabla resultado
+const div1 = document.getElementById('div1');
+    div1.innerHTML = '';
   const resultadoTable = document.getElementById('resultado');
   resultadoTable.innerHTML = '';
   const newRow = resultadoTable.insertRow();
@@ -94,15 +180,67 @@ function mostrarInv(){
       modificarButton.classList.add("modBtn");
       eliminarButton.textContent = "Eliminar";
       modificarButton.textContent = "Modificar";
+
       eliminarButton.addEventListener("click", () => {
-           
-          
       resultadoTable.deleteRow(row.rowIndex);            
       inventario.splice(index, 1);            
       localStorage.setItem("Inmueble", JSON.stringify(inventario));
       });
+
        modificarButton.addEventListener("click", () => {
+        Swal.fire({
+          title: 'Modificar Producto',
+          html: `
+          <div><label for="nombre">Nombre Inmueble: </label>
+          <input id="nombre" type="text" required value="${producto.nombre}">
+         </div> <br>
+          <div><label for="marca">Marca:</label>
+          <input id="marca" type="text" required value="${producto.marca}"></div>
+          <br>
+         <div> <label for="stock">Stock:</label>
+          <input id="stock" type="number" step="1" required value="${producto.stock}"></div>
+          <br>
+         <div> <label for="precio">Precio:</label>
+          <input id="precio" type="number" step="1" required value="${producto.precio}"></div>
+          <br>
+         <div> <label for="lugar">Lugar:</label>
+          <input id="lugar" type="text" required value="${producto.lugar}"></div>
+          <br>
+         <div>  <label for="observacion">Observación:</label>
+          <textarea id="observacion" rows="2" style="resize: none;" required value="${producto.observacion}"></textarea></div>
+          <br>
+          `,
+          showCancelButton: true,
+          confirmButtonText: 'Aceptar',
+          cancelButtonText: 'Cancelar',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            const newNombre = document.getElementById('nombre').value;
+            const newMarca = document.getElementById('marca').value;
+            const newStock = document.getElementById('stock').value;
+            const newPrecio = document.getElementById('precio').value;
+            const newLugar = document.getElementById('lugar').value;
+            const newObservacion = document.getElementById('observacion').value;
+
+            producto.nombre = newNombre;
+            producto.marca = newMarca;
+            producto.stock = newStock;
+            producto.precio = newPrecio;
+            producto.lugar = newLugar;
+            producto.observacion = newObservacion;
+
+            nombreCell.textContent = newNombre;
+            marcaCell.textContent = newMarca;
+            stockCell.textContent = newStock;
+            precioCell.textContent = newPrecio;
+            lugarCell.textContent = newLugar;
+            observacionCell.textContent = newObservacion;
+
+            localStorage.setItem("Inmueble", JSON.stringify(inventario));
+          }
+        });
           
+        
       });
         eliminarCell.appendChild(eliminarButton);
         eliminarCell.appendChild(modificarButton);
@@ -120,7 +258,8 @@ function filtrarInventario(){
   const palabraClave = input.trim().toUpperCase();
   const resultado = inventario.filter((producto) => producto.nombre.toUpperCase().includes(palabraClave));
 console.log(resultado)
-
+const div1 = document.getElementById('div1');
+    div1.innerHTML = '';
     const resultadoTable = document.getElementById('resultado');
     resultadoTable.innerHTML = ''; 
     const newRow = resultadoTable.insertRow();
@@ -168,7 +307,6 @@ console.log(resultado)
           }
           }
       
-
 /////////////////////////////////////////////////////////////////////////
 
     function agregarProducto() {       
@@ -181,27 +319,28 @@ console.log(resultado)
         formModal.style.display = 'none';
         form.remove();
     });
-
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
 const form = document.createElement('form')
     form.innerHTML = 
     `
-      <div><label for="nombre-input">Nombre Inmueble: </label>
+      <div class="addlabel"><label for="nombre-input">Nombre Inmueble: </label>
       <input id="nombre-input" type="text" required>
      </div> <br>
-      <div><label for="marca-input">Marca:</label>
+      <div class="addlabel"><label for="marca-input">Marca: </label>
       <input id="marca-input" type="text" required></div>
       <br>
-     <div> <label for="stock-input">Stock:</label>
+     <div class="addlabel"> <label for="stock-input">Stock: </label>
       <input id="stock-input" type="number" step="1" required></div>
       <br>
-     <div> <label for="precio-input">Precio:</label>
+     <div class="addlabel"> <label for="precio-input">Precio: </label>
       <input id="precio-input" type="number" step="1" required></div>
       <br>
-     <div> <label for="lugar-input">Lugar:</label>
+     <div class="addlabel"> <label for="lugar-input">Lugar: </label>
       <input id="lugar-input" type="text" required></div>
       <br>
-     <div>  <label for="observacion-input">Observación:</label>
-      <textarea id="observacion-input" rows="2" style="resize: none;" required></textarea></div>
+     <div class="addlabel">  <label for="observacion-input">Observación: </label>
+      <input id="observacion-input" rows="2" style="resize: none;" required></div>
       <br>
      <button type="submit">Agregar</button>
       <button id="cancelar">Cancelar</button>
@@ -303,5 +442,148 @@ const div1 = document.querySelector(".modal-content")
         
       });             
         }
+///////////////////////////////////////////////////////////////////
+    
+      
+fetch('https://mindicador.cl/api')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(dailyIndicators) {
+    // Create an array of indicator names and values
+    const indicators = [
+      { name: 'UF', value: dailyIndicators.uf.valor },
+      { name: 'Dólar observado', value: dailyIndicators.dolar.valor },
+      { name: 'Dólar acuerdo', value: dailyIndicators.dolar_intercambio.valor },
+      { name: 'Euro', value: dailyIndicators.euro.valor },
+      { name: 'UTM', value: dailyIndicators.utm.valor },
+      { name: 'UF', value: dailyIndicators.uf.valor },
+      { name: 'Dólar observado', value: dailyIndicators.dolar.valor },
+      { name: 'Dólar acuerdo', value: dailyIndicators.dolar_intercambio.valor },
+      { name: 'Euro', value: dailyIndicators.euro.valor },
+      { name: 'UTM', value: dailyIndicators.utm.valor },
+    ];
+    
+    indicators.forEach(function(indicator, index) {
+      const indicatorDiv = document.querySelector(`.slide${index + 1}`);
+      indicatorDiv.innerHTML = `
+        <div class="indicator-name">${indicator.name} <br> $${indicator.value}</br></div>
+           `;
+    });
 
+
+  })
+  .catch(function(error) {
+    console.log('Solicitud fallida', error);
+  });
+
+
+  function totalInmuebles(){   
    
+   const resultadoTable = document.getElementById('resultado');
+    resultadoTable.innerHTML = '';
+   
+    let totalPrecio = 0;
+    inventario.forEach((item) => {
+      const itemPrecio = parseFloat(item.precio);
+      if (!isNaN(itemPrecio)) {
+        totalPrecio += itemPrecio;
+      }
+    });
+  
+    const grafico = document.createElement('div');
+    grafico.innerHTML = `
+      <h1>Valor total de activos: $${totalPrecio}</h1>
+          `;
+  
+    resultado.appendChild(grafico);
+ 
+  }
+
+  function totalStock(){   
+    const resultadoTable = document.getElementById('resultado');
+     resultadoTable.innerHTML = '';
+    
+     let totalStock = 0;
+     inventario.forEach((item) => {
+       const itemStock = parseFloat(item.stock);
+       if (!isNaN(itemStock)) {
+         totalStock += itemStock;
+       }     
+           });
+     const grafico = document.createElement('div');
+     grafico.innerHTML = `
+       <h1>Total de items: ${totalStock}</h1>
+           `;
+   
+     resultado.appendChild(grafico);
+    
+   }
+
+
+  function ultimosAgregados(){
+
+    
+    const resultadoTable = document.getElementById('resultado');
+
+    resultadoTable.innerHTML = '';
+    if (inventario.length > 2) {
+      let ultimosDosInmuebles = [inventario[inventario.length - 2], inventario[inventario.length - 1]];
+    
+      let tablaHtml = '<table><tr>';
+      for (const x in ultimosDosInmuebles[0]) {
+        tablaHtml += `<th>${x}</th>`;
+      }
+      tablaHtml += '</tr>';
+    
+      for (const bien of ultimosDosInmuebles) {
+        tablaHtml += '<tr>';
+        for (const x in bien) {
+          tablaHtml += `<td>${bien[x]}</td>`;
+        }
+        tablaHtml += '</tr>';
+      }
+    
+      tablaHtml += '</table>';
+    
+      // Luego, asigna la tablaHtml al elemento HTML donde deseas mostrar la tabla
+      document.getElementById('resultado').innerHTML = tablaHtml;
+    } else {
+      Swal.fire({
+      icon: 'warning',
+      title: 'Advertencia',
+      text: 'No hay suficientes elementos en el inventario para mostrar los últimos 2.'
+    });
+    }
+  }
+  
+  function panel(){
+    // inicializo divs
+    const div1 = document.getElementById('div1');
+    div1.innerHTML = '';
+    const resultadoTable = document.getElementById('resultado');
+    resultadoTable.innerHTML = '';
+
+    const btnTotalInmuebles = document.createElement("button");
+    btnTotalInmuebles.textContent = "Total Inmuebles";
+    btnTotalInmuebles.addEventListener("click", totalInmuebles);
+    btnTotalInmuebles.classList.add("botonpanel");
+
+    const btnTotalStock = document.createElement("button");
+    btnTotalStock.textContent = "Total Stock";
+    btnTotalStock.addEventListener("click", totalStock);
+    btnTotalStock.classList.add("botonpanel");
+
+    const btnUltimosAgregados = document.createElement("button");
+    btnUltimosAgregados.textContent = "Últimos Agregados";
+    btnUltimosAgregados.addEventListener("click", ultimosAgregados);
+    btnUltimosAgregados.classList.add("botonpanel");
+    div1.appendChild(btnTotalInmuebles);
+    div1.appendChild(btnTotalStock);
+    div1.appendChild(btnUltimosAgregados);
+   
+ 
+
+  }
+  
+  
